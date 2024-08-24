7ae85497d2cf9765c03b9e12bd19889adb06ff66
Theo Reeves <theo_25@icloud.com>
2024-08-24 11:43:25 +0100

    changed font and updated styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-// import { useState } from "react";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Form from "./pages/Form";
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
       <h1>F1 Fan Track Messages</h1>
       <div id="navBox">
         <nav className="navBar">
-          <Link to="/">Home </Link>
+          <Link to="/"> Home </Link>
           <Link to="/messages"> Messages</Link>
           <Link to="/form"> Form </Link>
         </nav>
